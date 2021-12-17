cde467b32be71919691e300faf29d00fbf05889b
Aaron <chillaaron21@gmail.com>
2021-12-17 12:18:53 -0500

    Add project files.

---
+++ DojoSurveyWithValidations/Models/Dojo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DojoSurveyWithValidations.Models
+{
+    public class Dojo
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Name must be longer than 2 Characters")]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [MinLength(20, ErrorMessage = "Comment must be longer than 20 chars")]
+        public string Comment { get; set; }
+    }
+}
